ded6d3ef9790c3c9037005c87b6f45e7c9d84822
Gordon H Graham <17663335+gordonhgraham@users.noreply.github.com>
2019-08-06 21:34:48 -0600

    updates SeriesSearch to use SearchForm.jsx

--- src/components/SeriesSearch.jsx
+++ src/components/SeriesSearch.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import SearchForm from "./SearchForm";
 
 export default class SeriesSearch extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.handleSearch = this.handleSearch.bind(this);
+    this.state = {
+      displayContent: ""
+    };
+  }
+
+  handleSearch(searchTerm) {
+    // do axios request logic here (maybe start w/ request-promise & switch to axios)
+    // need to use search Term as a query parameter
+
+    return this.setState({ displayContent: searchTerm });
   }
 
   render() {
@@ -12,6 +23,9 @@ export default class SeriesSearch extends Component {
       <>
         <h1>Series Search</h1>
         <button onClick={() => switchToMenu()}>Back To Menu</button>
+        <SearchForm handleSearch={this.handleSearch} />
+        <h3>axios request w/ following params:</h3>
+        <h1>{this.state.displayContent || "..."}</h1>
       </>
     );
   }
