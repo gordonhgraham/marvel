a84c38f1c5dd80c8c6f37647163272c99083b369
Gordon H Graham <17663335+gordonhgraham@users.noreply.github.com>
2019-08-06 20:23:00 -0600

    adds form to characterSearch

--- src/CharacterSearch.jsx
+++ src/CharacterSearch.jsx
@@ -3,7 +3,20 @@ import React, { Component } from "react";
 export default class CharacterSearch extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = {
+      searchInput: ""
+    };
+  }
+
+  handleInputChange(event) {
+    this.setState({ searchInput: event.target.value });
+  }
+
+  handleSearch(event) {
+    event.preventDefault();
+
+    // search w/ this.state.searchInput
   }
 
   render() {
@@ -12,6 +25,17 @@ export default class CharacterSearch extends Component {
       <>
         <h1>Character Search</h1>
         <button onClick={() => switchToMenu()}>Back To Menu</button>
+        <form>
+          <input
+            type="text"
+            placeholder="Search a character"
+            value={this.state.searchInput}
+            onChange={e => {
+              this.handleInputChange(e);
+            }}
+          />
+          <button onClick={e => this.handleSearch(e)}>Search</button>
+        </form>
       </>
     );
   }
