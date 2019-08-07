efa6977ab193f8a5efdfd069fc2ee455bf95c195
Gordon H Graham <17663335+gordonhgraham@users.noreply.github.com>
2019-08-06 23:24:50 -0600

    adds axios call to CharacterSearch

--- src/components/CharacterSearch.jsx
+++ src/components/CharacterSearch.jsx
@@ -1,32 +1,57 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchForm from "./SearchForm";
+import { marvelApiKey } from "../config";
+import { log } from "handlebars";
 
 export default class CharacterSearch extends Component {
   constructor(props) {
     super(props);
     this.handleSearch = this.handleSearch.bind(this);
     this.state = {
-      displayContent: ""
+      displayContent: {
+        name: "",
+        description: "",
+        thumbnail: {}
+      }
     };
   }
 
   handleSearch(searchTerm) {
-    // do axios request logic here (maybe start w/ request-promise & switch to axios)
-    // need to use search Term as a query parameter
-
-    return this.setState({ displayContent: searchTerm });
+    axios
+      .get(
+        `https://gateway.marvel.com:443/v1/public/characters?name=${searchTerm}&apikey=${marvelApiKey}`
+      )
+      .then(res => {
+        this.setState({ displayContent: res.data.data.results[0] });
+      })
+      .catch(error => {
+        console.log("<=> <=><=> <=> <=> <=><=> <=> <=> <=><=> <=>");
+        console.log("bummer, an error... ", error);
+        console.log("<=> <=><=> <=> <=> <=><=> <=> <=> <=><=> <=>");
+      });
   }
 
   render() {
     const { switchToMenu } = this.props;
+    const {
+      name,
+      description,
+      thumbnail,
+      thumbnail: { path, extension }
+    } = this.state.displayContent;
     return (
       <>
         <h1>Character Search</h1>
         <button onClick={() => switchToMenu()}>Back To Menu</button>
         <SearchForm handleSearch={this.handleSearch} />
-        <h3>axios request w/ following params:</h3>
-        <h1>{this.state.displayContent || "..."}</h1>
+        <h1>{name}</h1>
+        <h3>{description}</h3>
       </>
     );
   }
 }
+
+// {thumbnail.length > 0 ? (
+//   <img src={`${path}.${extension}`} className="App-logo" alt="logo" />
+//     ) : null}
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  marvelApiKey: "a11aa85634381aba06611f0f575de5b3"
+};
